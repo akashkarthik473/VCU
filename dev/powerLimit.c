cd48a2f0a4b6591f11cdb01b086df37470ed191b
carlieyem <carlieyem@yahoo.com>
2025-06-20 07:44:01 -0700

    Practice on Test Track 75kwh

--- dev/powerLimit.c
+++ dev/powerLimit.c
@@ -36,10 +36,11 @@ PowerLimit* POWERLIMIT_new(){
     //4. Both TQ equation and LUT together-(Final Algorithm)
     me->plStatus = FALSE;
     me->plTorqueCommand = 0; 
-    me->plTargetPower = 50;// HERE IS WHERE YOU CHANGE POWERLIMIT
+    me->plTargetPower = 75;// HERE IS WHERE YOU CHANGE POWERLIMIT
     me->plThresholdDiscrepancy = 15;
     me->plInitializationThreshold = 0;
     me->clampingMethod = 3;
+
     me->plAlwaysOn = TRUE;
     //LUT Corners
     me->vFloorRFloor = 0;
